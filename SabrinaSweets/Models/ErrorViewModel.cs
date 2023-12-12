fd295bacec76da0616e8805e53e2c4f44db5146a
RockArtist33 <jack-ogrady@outlook.com>
2023-12-12 13:20:28 +0000

    vsaduf

---
+++ SabrinaSweets/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SabrinaSweets.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
