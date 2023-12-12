b1f7e237f3fac230173efb6a1a83ce125b32fcbb
RockArtist33 <jack-ogrady@outlook.com>
2023-12-12 14:55:53 +0000

    wetasery

--- SabrinaSweets/Data/Migrations/20231212132717_InitialCreate.Designer.cs
+++ SabrinaSweets/Data/Migrations/20231212132717_InitialCreate.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SabrinaSweets.Data;
 
@@ -11,9 +12,10 @@ using SabrinaSweets.Data;
 namespace SabrinaSweets.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231212132717_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ SabrinaSweets/Models/ShoppingItem.cs
@@ -0,0 +1,15 @@
+﻿namespace SabrinaSweets.Models
+{
+    public class ShoppingItem
+    {
+        public string ShoppingItemId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Size { get; set; }
+        public float price { get; set; }
+        public string TagId { get; set; }
+        public string Ingredients { get; set; }
+        public string Nutritional_Values { get; set; }
+        public string? Dietary { get; set; }
+    }
+}
