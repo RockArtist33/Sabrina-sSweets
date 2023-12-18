31efb2a2cd762a1b0316bd3f9486b8e087628254
pencynical <148549428+pencynical@users.noreply.github.com>
2023-12-18 09:50:13 +0000

    changes

--- SabrinaSweets/Models/Seeddata.cs
+++ SabrinaSweets/Models/Seeddata.cs
@@ -1,6 +1,26 @@
-﻿namespace SabrinaSweets.Models
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using SabrinaSweets.Data;
+using System;
+using System.Linq;
+
+
+namespace SabrinaSweets.Models
 {
-    public class Seeddata
+    public static class Seeddata
     {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new ApplicationDbContext(
+                serviceProvider.GetRequiredService<DbContextOptions<ApplicationDbContext>>()))
+            {
+                if (context.Setting.Any())
+                {
+                    return;
+                }
+
+            }
+        }
+
     }
 }
